20b59df4f999a26ef0646a3214865795594f5839
meng guoru <meng guoru>
2017-06-16 15:26:19 +0800

    rigidbody rotation

--- Assets/Scripts/my_CarController2.cs
+++ Assets/Scripts/my_CarController2.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class my_CarController2 : MonoBehaviour {
     Rigidbody rb;
     public float forwardSpeed = 40f;
+    //rotate
+    public float rotateFactor = 100f;
+    bool isGrounded;
     public void Start()
     {
         rb = GetComponent<Rigidbody>();
+        //print(rb.angularVelocity);
+        isGrounded = false;
     }
     public void Move(float hori,float veri)
     {
-        Vector3 force = transform.forward * forwardSpeed * veri;
-        rb.AddForce(force);
+        if(isGrounded)
+        {
+            Vector3 force = transform.forward * forwardSpeed * veri;
+            rb.AddForce(force);
+
+            //Quaternion deltaRotation = Quaternion.Euler();
+            //rb.MoveRotation(rb.rotation)
+            rb.AddTorque(transform.up * rotateFactor * hori, ForceMode.Force);
+        }
+    }
+    void OnCollisionStay()
+    {
+        isGrounded = true;
     }
 }
