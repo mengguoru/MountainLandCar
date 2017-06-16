02f7799f1bea1a7226f4eb2d9b1df1fb740ef792
meng guoru <meng guoru>
2017-06-16 08:52:52 +0800

    start modify player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour {
     public float Zspeed = 150f;
+    public float torque = 4f;
 
     public Vector3 jump;
     public float jumpForce = 2.0f;
@@ -19,12 +20,12 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * Zspeed;
+        // var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        // //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        // var z = Input.GetAxis("Vertical") * Time.deltaTime * Zspeed;
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        // transform.Rotate(0, x, 0);
+        // transform.Translate(0, 0, z);
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
@@ -32,6 +33,21 @@ public class Player : MonoBehaviour {
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        //Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        //todo,rotate
+
+        Vector3 force = moveVertical * Zspeed * transform.forward;
+        rb.AddForce(force);
+        transform.Rotate(0, moveHorizontal, 0);
+        //rb.AddTorque(transform.up * torque * moveHorizontal);
+        //rb.velocity = movement * Zspeed;
     }
 
     //control jump
