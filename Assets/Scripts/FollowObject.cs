931563b1704da3e184af5da2658213b9772f5db7
meng guoru <meng guoru>
2017-06-20 11:13:25 +0800

    change minimap

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour {
+    public Transform target;
+	// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 tmp = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = tmp;
+	}
+}
