630bf8be1c9b7f7e0e6dbabc86a32bddef1cfe76
meng guoru <meng guoru>
2017-06-20 17:40:30 +0800

    start ui

---
+++ Assets/Scripts/UImanager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UImanager : MonoBehaviour {
+
+	// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+    public void level1()
+    {
+        SceneManager.LoadScene("level1");
+    }
+    public void exit()
+    {
+        SceneManager.LoadScene("level1");
+    }
+}
