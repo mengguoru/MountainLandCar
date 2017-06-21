1444b6fb7a560913e2c550fa6a2086344e38d569
meng guoru <meng guoru>
2017-06-21 16:33:05 +0800

    add endpoint

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        if ("endpoint" == other.name)
+            SceneManager.LoadScene("level2");
+    }
+}
