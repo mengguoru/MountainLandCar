d4936ab70e05e9465fdeb17244daa6baf827a7f6
meng guoru <meng guoru>
2017-06-22 16:17:57 +0800

    clean

---
+++ Assets/Scripts/level1Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level1Manager : MonoBehaviour {
+    public GameObject[] m_objs;
+    bool m_start = false;
+	// Use this for initialization
+	void Start () {
+        m_start = false;
+        foreach (var i in m_objs)
+            i.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+            m_start = true;
+        if(m_start)
+        {
+            foreach (var i in m_objs)
+                i.SetActive(true);
+        }
+	}
+}
