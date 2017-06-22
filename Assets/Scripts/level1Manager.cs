2105e33f21c0f4ee1bd799737d2d78300cd679f9
meng guoru <meng guoru>
2017-06-22 16:30:25 +0800

    text fade

--- Assets/Scripts/level1Manager.cs
+++ Assets/Scripts/level1Manager.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class level1Manager : MonoBehaviour {
     public GameObject[] m_objs;
+    public GameObject[] m_disableObjs;
+    public Text m_txt;
     bool m_start = false;
 	// Use this for initialization
 	void Start () {
         m_start = false;
         foreach (var i in m_objs)
             i.SetActive(false);
-	}
+        //fade text
+        TextFadeController tmp = GetComponent<TextFadeController>();
+        StartCoroutine(tmp.FadeTextToZeroAlpha(5f, m_txt));
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,6 +26,9 @@ public class level1Manager : MonoBehaviour {
         {
             foreach (var i in m_objs)
                 i.SetActive(true);
+            foreach(var i in m_disableObjs)
+                i.SetActive(false);
+            //m_txt.SetActive(false);
         }
 	}
 }
