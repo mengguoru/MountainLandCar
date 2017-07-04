e03fb7c7b7a37f2961c0cc253d94fdfecd60175b
XingYe1 <731406982@qq.com>
2017-07-04 09:46:19 +0800

    7-4

---
+++ Assets/test/Timmer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timmer : MonoBehaviour {
+
+    public GameObject textField;
+    public int timeLeft = 90;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Count());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Count()
+    {
+        while (timeLeft >= 0)
+        {
+            textField.GetComponent<Text>().text = timeLeft.ToString();
+        
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+        GameOver();
+    }
+    void GameOver()
+    {
+
+    }
+}
