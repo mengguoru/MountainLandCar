a529197b6c0d8787c3349b4a637f6a0f7449e1a3
meng guoru <meng guoru>
2017-07-05 15:27:28 +0800

    gascan prop

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.Vehicles.Car;
 
 public class Player : MonoBehaviour {
 
@@ -19,5 +20,22 @@ public class Player : MonoBehaviour {
         //for scene2 
         if ("endpoint" == other.name)
             SceneManager.LoadScene("level3");
+        if("oil" == other.tag)
+        {
+            Debug.Log("oil");
+            Destroy(other.transform.root.gameObject);
+            getOil();
+        }
+    }
+    public void getOil()
+    {
+        if(GetComponent<CarController>() != null)
+        {
+            if (GetComponent<Rigidbody>() != null)
+            {
+                //GetComponent<Rigidbody>().velocity = GetComponent<CarController>().MaxSpeed;
+                GetComponent<Rigidbody>().velocity *= 5;
+            }
+        }
     }
 }
